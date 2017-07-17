5b3ebf8013ac8b53e0c4be0ff1b70dda780a7785
mapcic <mapcic@mail.ru>
2017-07-17 17:42:24 +0300

    Add INSERT DB

--- init.php
+++ init.php
@@ -1,4 +1,6 @@
 <?php 
 	$db = JFactory::getDbo();
-	$query = "CREA";	
+	$query = "CREATE TABLE IF NOT EXISTS `#__mcomments_0` ( `id` int(11) NOT NULL AUTO_INCREMENT, `email` varchar(255) NOT NULL, `message` mediumtext NOT NULL, `parent` int(11) DEFAULT 0, `utime` int(11) DEFAULT 0, PRIMARY KEY (`id`) );";
+	$db->setQuery($query);
+	$db->query();
 ?>
--- init.sql
+++ init.sql
@@ -13,3 +13,8 @@ CREATE TABLE IF NOT EXISTS `#__mcomments_ids` (
 	`table_name` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
+
+-- Inline
+CREATE TABLE IF NOT EXISTS `#__mcomments_<id>` ( `id` int(11) NOT NULL AUTO_INCREMENT, `email` varchar(255) NOT NULL, `message` mediumtext NOT NULL, `parent` int(11) DEFAULT 0, `utime` int(11) DEFAULT 0, PRIMARY KEY (`id`) );
+
+CREATE TABLE IF NOT EXISTS `#__mcomments_ids` ( `id` int(11) NOT NULL AUTO_INCREMENT, `home` int(1) DEFAULT 0, `table_name` varchar(255) NOT NULL, PRIMARY KEY (`id`) );
