82a5b2965209d45eb019bb18730b97cfede04f3f
mapcic <mapcic@mail.ru>
2017-07-18 12:05:58 +0300

    Add Init.php

--- init.php
+++ init.php
@@ -1,6 +1,39 @@
-<?php 
+<?php
+function mCommetntsInit(){
 	$db = JFactory::getDbo();
-	$query = "CREATE TABLE IF NOT EXISTS `#__mcomments_0` ( `id` int(11) NOT NULL AUTO_INCREMENT, `email` varchar(255) NOT NULL, `message` mediumtext NOT NULL, `parent` int(11) DEFAULT 0, `utime` int(11) DEFAULT 0, PRIMARY KEY (`id`) );";
-	$db->setQuery($query);
-	$db->query();
+
+	$query = $db->getQuery(true)
+		->select(array('id', 'path', 'home'))
+		->from($db->qn('#__menu'))
+		->where($db->qn('published').' = 1 AND '.$db->qn('link').' LIKE "%option=com_content%"');
+	$pages = $db->setQuery($query)
+		->loadObjectList();
+
+	foreach ($pages as $key => $val) {
+		$page = (object) array(
+			'table_name' => '#__mcomments_'.$val->id,
+			'path' => $val->path,
+			'home' => $val->home
+		);
+
+		$query = $db->getQuery('true')
+			->select($db->qn('id'))
+			->from($db->qn('#__mcomments_ids'))
+			->where($db->qn('path').' = '.$page->path);
+		$resp = $db->setQuery($query)
+			->loadResult();
+
+		if (empty($resp)) {
+			return 1;
+		}
+
+		if (!$db->insertObject('#__mcomments_ids', $page)) {
+			return -1;
+		}
+
+		$query = 'CREATE TABLE IF NOT EXISTS `'.$page->table_name.'` ( `id` int(11) NOT NULL AUTO_INCREMENT, `email` varchar(255) NOT NULL, `message` mediumtext NOT NULL, `parent` int(11) DEFAULT 0, `utime` int(11) DEFAULT 0, PRIMARY KEY (`id`) );';
+		$db->setQuery($query)
+			->query();
+	}
+}
 ?>
--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `#__mcomments_<id>` (
+sCREATE TABLE IF NOT EXISTS `#__mcomments_<id>` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`email` varchar(255) NOT NULL,
 	`message` mediumtext NOT NULL,
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS `#__mcomments_<id>` (
 CREATE TABLE IF NOT EXISTS `#__mcomments_ids` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`home` int(1) DEFAULT 0,
+	`path` varchar(255) NOT NULL,
 	`table_name` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
