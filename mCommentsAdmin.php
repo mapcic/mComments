7a4fef7c2e6d3ee782396ffa5cf485c0488d510e
mapcic <mapcic@mail.ru>
2017-08-03 14:31:13 +0300

    Fix.

--- mCommentsAdmin.php
+++ mCommentsAdmin.php
@@ -1,17 +1,18 @@
-<?php
-$user = JFactory::getUser();
-$isroot = $user->authorise('core.login.admin');
-
-if ($isRoot == 1) {
-	// Подгрузка библиотеки и ее инициализация
-	mCommentsAdminInit();
-}
-?>
-
 <?php 
 function mCommentsAdminInit($value='') {
 	mcGetLast();
 }
 
+function addComment() {
+
+}
+
+function rmComment() {
+
+}
+
+function getComment() {
+
+}
 
 ?>
