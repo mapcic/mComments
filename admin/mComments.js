de290a767582cedf4ff75cc121eec6b15e2c617f
mapcic <mapcic@mail.ru>
2017-08-03 17:45:16 +0300

    Fix

---
+++ admin/mComments.js
@@ -0,0 +1,53 @@
+function mc_init() {
+
+}
+
+function load_mc(event) {
+	event.preventDefault();
+
+	var $this = jQuery(this),
+		node = $this.parents('.mComments'),
+		info = node.find('.mcInfo');
+
+	jQuery.ajax(
+		type : 'POST', url : '/templates/protostar/php/mCommentsAdmin.php', dataType: 'json', 
+		data: { offset: info.attr('offset'), num: info.attr('num'), table: info.attr('table'), method: 'get' },
+		success: function(data) {
+			// info.attr('offset', data.offset).attr('len', data.len);
+			if (data.offset >= data.len) {
+				$this.addClass('ShliambOff').off('click', load_mc);
+			}
+
+			jQuery.each(data.items, function(ind, val) {
+				var div = '<div class="mcComment mcLevel'+val.level+'" mcid="'+val.id+'" level="'+val.level+'" branchId="'+val.branchId+'">' +
+							'<div class="mcEmail">'+val.email+'</div>' +
+							'<div class="mcTime">'+val.utime+'</div>' +
+							'<div class="mcMessаge">'+val.message+'</div>' +
+							'<div class="mcAnswer">Ответить</div>' +
+						'</div>',
+					comments = node.find('.mcComments');
+				jQuery(div).appendTo(comments).find('.mcAnswer').on('click', answer_mc);
+			});
+		}
+	);
+}
+
+function remove_mc( event ) {
+	event.preventDefault();
+
+	var $this = jQuery(this),
+		comment = $this.parent(),
+		info = $this.parents('.mComments').find('.mcInfo');
+	jQuery.ajax(
+		type : 'POST', url : '/templates/protostar/php/mCommentsAdmin.php', dataType: 'json', 
+		data: { id : comment.attr('mcid'), table: info.attr('table'), method: 'remove' },
+		success: function(data) {
+			info.attr('offset', +info.attr('offset')-1).attr('len', +info.attr('len')-1);
+			comment.remove();
+		}
+	);
+}
+
+function add_mc() {
+	
+}
--- admin/mComments.php
+++ admin/mComments.php
@@ -1,8 +1,4 @@
 <?php 
-function mCommentsAdminInit($value='') {
-	mcGetLast();
-}
-
 function addComment() {
 
 }
